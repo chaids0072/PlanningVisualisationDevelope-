e320abb9a6b313021f08a08098835375a7577c9f
Gang_Chen <gche8512@Gmail.com>
2018-08-06 00:31:03 +1000

    Update integration_testing_step1.py

--- Test/integration-test/src/block/integration_testing_step1.py
+++ Test/integration-test/src/block/integration_testing_step1.py
@@ -3,24 +3,24 @@ sys.path.append('../../../../server/PddLparser/visualiserFile/PredicateParser')
 import unittest
 import plan_generator as step1
 
-# Test if the domain file is not empty
-def test_integration__input_domain_step_1(self):
-    with open("../release1.0/domain_blocks.pddl", "r") as f1:
-        domaintext = f1.read()
-    self.assertTrue(domaintext)
-    f1.close()
+class integration_testing_step1(unittest.TestCase):
+    # Test if the domain file is not empty
+    def test_integration__input_domain_step_1(self):
+        with open("../../input/domain_blocks.pddl", "r") as f1:
+            domaintext = f1.read()
+        self.assertTrue(domaintext)
+        f1.close()
 
-# Test if the problem file is not empty
-def test_integration__input_problem_step_1(self):
-    with open("../release1.0/problems/bw01.pddl", "r") as f2:
-        problemtext = f2.read()
-    self.assertTrue(problemtext)
-    f2.close()
+    # Test if the problem file is not empty
+    def test_integration__input_problem_step_1(self):
+        with open("../../input/bw08.pddl", "r") as f2:
+            problemtext = f2.read()
+        self.assertTrue(problemtext)
+        f2.close()
 
-# Test if the returning value from the API is not empty
-def test_integration_api_result(self):
-    self.assertTrue(step1.get_plan("../release1.0/domain_blocks.pddl", "../release1.0/problems/bw01.pddl"))
-
+    # Test if the returning value from the API is not empty
+    def test_integration_api_result(self):
+        self.assertTrue(step1.get_plan("../../input/domain_blocks.pddl", "../../input/bw08.pddl"))
 
 if __name__ == "__main__":
     unittest.main()
